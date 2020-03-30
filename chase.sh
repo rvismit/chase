c85d0356766f383fc8bde959ab2b36283ad2a5c9
rvismit <rvismit@gmail.com>
2020-03-30 12:30:26 +0530

    Add files via upload

---
+++ chase.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Author: DRUK
+# Assist: https://github.com/rvismit/chase
+# twitter: https://www.twitter.com/rvismit
+# Created Mar 2020 | Monastery
+# New updates coming soon.
+
+echo "_________   ___ ___    _____    ____________________ "
+echo "\_   ___ \ /   |   \  /  _  \  /   _____/\_   _____/ "
+echo "/    \  \//    ~    \/  /_\  \ \_____  \  |    __)_  "
+echo "\     \___\    Y    /    |    \/        \ |        \ "
+echo " \______  /\___|_  /\____|__  /_______  //_______  /  "
+echo "        \/       \/         \/        \/         \/   "
+
+echo -en '\n'
+
+BROWSER="xdg-open" #Open-Browser
+
+DELAY=10
+
+echo  "Hello ! Hope you will find a good program and bounties too.
+
+#Google Dorks
+
+$BROWSER https://www.google.com/search?q=inurl:/.well-known/security+ext:txt
+$BROWSER https://www.google.com/search?q=site:inurl:reporting-security-issues
+$BROWSER https://www.google.com/search?q="submit+vulnerability+report"
+$BROWSER https://www.google.com/search?q=site:help.*.*+inurl:bounty
+$BROWSER https://www.google.com/search?q=inurl:/security+ext:txt+"contact"
+
+sleep $DELAY
+
+$BROWSER https://www.google.com/search?q=responsible+disclosure:sites
+$BROWSER https://www.google.com/search?q=inurl:security.txt
+$BROWSER https://www.google.com/search?q=responsible+disclosure+white+hat
+$BROWSER https://www.google.com/search?q="powered+by+bugcrowd"+-site:bugcrowd.com
+$BROWSER https://www.google.com/search?q=inurl:/.well-known/security+ext:txt+intext:hackerone
+
+sleep $DELAY
+
+$BROWSER https://www.google.com/search?q=inurl:security-policy.txt+ext:txt
+$BROWSER https://www.google.com/search?q=inurl:/security ext:txt+"contact"
+$BROWSER https://www.google.com/search?q=site:*.*.nl intext:responsible+disclosure+reward
+$BROWSER https://www.google.com/search?q=site:responsibledisclosure.com
+$BROWSER https://www.google.com/search?q=inurl+:+/+security
